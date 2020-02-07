98fadfe72bbd01d60276433d1efe13cac1933cf3
Carl Montemayor <carl.n.montemayor@gmail.com>
2020-02-06 20:50:12 -0800

    added pop-up box exclusion

--- Solution/core.py
+++ Solution/core.py
@@ -7,6 +7,7 @@ browser.get('https://www.allegiantair.com')
 
 assert 'Allegiant' in browser.title
 
-
+delBox = browser.find_element_by_css_selector("body > div.ui-dialog.ui-widget.ui-widget-content.ui-corner-all.ui-front.credit-card-overlay-wrapper.dialog-drupal.ui-draggable.ui-resizable > div.ui-dialog-titlebar.ui-widget-header.ui-corner-all.ui-helper-clearfix.ui-draggable-handle > button").click()
+browser.wait_implicitly(1000)
 
 browser.quit()
